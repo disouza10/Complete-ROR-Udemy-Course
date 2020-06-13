f25912ee340bd8309d941da7c2dc4695d9009f1d
Diego Souza <diegoshowza93@gmail.com>
2020-06-13 19:10:42 -0300

    finishing minimum product

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,18 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  post 'message', to: 'messages#create'
   
-  devise_for :users, path_names: {
-    sign_in: 'login', 
-    sign_out: 'logout', 
-    password: 'secret', 
-    confirmation: 'verification', 
-    unlock: 'unblock', 
-    registration: 'sign_up', 
-    sign_up: 'cmon_let_me_in'
-  }
+  # devise_for :users, path_names: {
+  #   sign_in: 'login', 
+  #   sign_out: 'logout', 
+  #   password: 'secret', 
+  #   confirmation: 'verification', 
+  #   unlock: 'unblock', 
+  #   registration: 'sign_up', 
+  #   sign_up: 'cmon_let_me_in'
+  # }
 
 end
